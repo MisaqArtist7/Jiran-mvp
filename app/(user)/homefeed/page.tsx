58e0aedae7e3cbc5e5e8909266f8c187fa5779cb
MisaqArtist7 <misaqbabakhani777@gmail.com>
2025-11-23 22:01:10 +0330

    create add post for homefeed

--- app/(user)/homefeed/page.tsx
+++ app/(user)/homefeed/page.tsx
@@ -1,14 +1,15 @@
 'use client'
+import Image from "next/image"
 import Link from "next/link"
 import { useState } from "react"
 export default function HoomFeedPage() {
   const [modal, setModal] = useState(false)
   return (
-    <section className="relative">
+    <section className="relative ">
       {/* homefeed section */}
-      <div className="flex justify-between">
+      <div className="relative flex justify-between">
         {/* asside section */}
-        <aside className="shadow bg-white border border-gray-100 w-72 p-4 h-screen">
+        <aside className="shadow bg-white border border-gray-100 min-w-72 px-4 py-7 fixed left-0 bottom-0 top-0">
           <div>
             <h1 className="text-2xl font-semibold pb-2 px-1">Home Feed || Jiran</h1>
             <ul className="flex flex-col justify-start gap-2">
@@ -43,50 +44,131 @@ export default function HoomFeedPage() {
           </div>
         </aside>
         {/* body section */}
-        <div>2</div>
+        <div className="flex justify-center w-full p-7 ml-72 ">
+          {/* posts and profile section */}
+          <div>
+            {/* profile info */}
+            <div className="flex flex-col items-start justify-center bg-amber-500/0">
+              <div className="flex items-center justify-start gap-3">
+                <Image src="/images/avatar.svg" height={136} width={136} alt=""></Image>
+                <div className="text-lg">
+                  <h2 className="text-2xl font-semibold">Misaq Artist</h2>
+                  <h3>posts: 7</h3>
+                  <h4>Front-end developer and software architecture</h4>
+                  <h5>Telegram: @misaqartist021</h5>
+                </div>
+              </div>
+              <div className="flex items-center justify-center gap-2">
+                <button className="flex-row-center px-11 py-1 border-2 border-(--primaryColor) rounded-lg mt-3 bg-white hover:bg-(--primaryColor) hover:text-white transition ease-in duration-75">Edit profile</button>
+                <button className="flex-row-center px-11 py-1 border-2 border-(--primaryColor) rounded-lg mt-3 bg-white hover:bg-(--primaryColor) hover:text-white transition ease-in duration-75">Notification</button>
+              </div>
+            </div>
+
+            {/* post info */}
+            <div className="my-11 bg-amber-600/0">
+              <div className="flex items-center justify-around gap-11 w-full">
+                <button>
+                  <svg className="w-8 h-8">
+                    <use href="#squareIcon"></use>
+                  </svg>
+                </button>
+                <button>
+                  <svg className="w-8 h-8">
+                    <use href="#squareIcon"></use>
+                  </svg>
+                </button>
+                <button>
+                  <svg className="w-8 h-8">
+                    <use href="#squareIcon"></use>
+                  </svg>
+                </button>
+                <button>
+                  <svg className="w-8 h-8">
+                    <use href="#squareIcon"></use>
+                  </svg>
+                </button>
+              </div>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center gap-3 my-3">
+                <div>
+                  <Image src="/images/post.jpg" width={250} height={250} alt="" className="rounded-xs" />
+                </div>
+                <div>
+                  <Image src="/images/post.jpg" width={250} height={250} alt="" className="rounded-xs" />
+                </div>
+                <div>
+                  <Image src="/images/post.jpg" width={250} height={250} alt="" className="rounded-xs" />
+                </div>
+                <div>
+                  <Image src="/images/post.jpg" width={250} height={250} alt="" className="rounded-xs" />
+                </div>
+                <div>
+                  <Image src="/images/post.jpg" width={250} height={250} alt="" className="rounded-xs" />
+                </div>
+                <div>
+                  <Image src="/images/post.jpg" width={250} height={250} alt="" className="rounded-xs" />
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
       </div>
 
       {/* modal button */}
-      <div className="fixed bottom-7 bg-(--primaryColor) right-7 text-white rounded-full p-3 cursor-pointer">
+      <div onClick={() => setModal((prevState) => !prevState)}
+      className="fixed bottom-7 bg-(--primaryColor) right-7 text-white rounded-full p-3 cursor-pointer">
         <svg className="w-9 h-9">
           <use href="#addIcon"></use>
         </svg>
       </div>
+
       {/* modal section */}
       {modal && (
         <div className="fixed inset-0 bg-black/40 flex items-center justify-center px-4">
-          <div className="bg-white p-5 pb-7 rounded-md w-full max-w-md">
+          <div className="bg-white p-5 rounded-lg w-full max-w-md">
             <div className="flex justify-between items-center mb-4">
               <h2 className="text-lg font-bold">Create Post</h2>
-              <button>
+              <button onClick={() => setModal((prevState) => !prevState)}>
                 <svg className="w-7 h-7">
                   <use href="#closeIcon"></use>
                 </svg>
               </button>
             </div>
-            <textarea
-              placeholder="Write something..."
-              className="w-full border p-2 rounded-lg mb-3"
-              rows={3}
-            />
+            <form action="">
+              <input
+                type="text"
+                placeholder="Title: for example: car"
+                className="w-full border-2 border-gray-300 outline-none hover:border-(--primaryColor) p-2 rounded-lg mb-3"
+              />
 
-            <input
-              type="file"
-              accept="image/*"
-              className="mb-3"
-            />
+              <textarea
+                placeholder="Description..."
+                className="w-full border-2 border-gray-300 outline-none hover:border-(--primaryColor) p-2 rounded-lg mb-3"
+                rows={3}
+              />
 
-          <select
-            className="w-full border p-2 rounded-lg mb-4"
-          >
-            <option value="">Select category</option>
-            <option value="news">News</option>
-            <option value="sport">Sport</option>
-          </select>
+              <input
+                type="text"
+                placeholder="Address..."
+                className="w-full border-2 border-gray-300 outline-none hover:border-(--primaryColor) p-2 rounded-lg mb-3"
+              />
 
-          <button className="w-full bg-(--primaryColor) text-white py-2 rounded-lg cursor-pointer">
-            Post
-          </button>
+              <input
+                type="file"
+                accept="image/*"
+                className="mb-3"
+              />
+              <select className="w-full border-2 border-gray-300 p-2 hover:border-(--primaryColor) rounded-lg mb-4 outline-none " >
+                <option value="">Select category</option>
+                <option value="stolen">stolen</option>
+                <option value="lost">lost</option>
+              </select>
+            </form>
+
+            <button className="w-full bg-(--primaryColor)/90 hover:bg-(--primaryColor) text-white py-2 rounded-lg cursor-pointer">
+              Post
+            </button>
 
           </div>
         </div>
--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import Header from "@/src/components/shared/Header/Header";
 export default function UserLayout({ children } : { children : React.ReactNode }){
     return(
         <main>
-            <Header /> 
             {children}
         </main>
     ) 
