b53789ef160ab15f303827fce1ecc423f42980de
MisaqArtist7 <misaqbabakhani777@gmail.com>
2025-12-03 11:37:06 +0330

    fix bug

--- app/(user)/edit-dashboard/page.tsx
+++ app/(user)/edit-dashboard/page.tsx
@@ -59,7 +59,7 @@ export default function DashboardEdit() {
                   </div>
                 </div>
             </div>
-            <form action="" className='flex flex-col justify-center gap-4 py-3 px-7'>
+            <form action="" className='flex flex-col justify-center gap-4 py-3 px-4'>
               <div className='flex items-center justify-start gap-7'>
                 <label htmlFor="" className='text-lg'>Name</label>
                 <input type="text" placeholder='MisaqArtist' className='w-1/4 border-b border-gray-300 py-1 px-2'/>
@@ -102,14 +102,69 @@ export default function DashboardEdit() {
           </div>
       </div>
 
-      <div className="bg-white shadow p-3 mx-3 ml-76 my-4">
-        <h2 className="flex items-center p-3 text-xl text-red-600">
+      <div className="bg-white shadow p-3 px-7 mx-3 ml-76 my-4">
+        <h2 className="flex items-center py-3 text-xl text-red-600">
           <svg className='w-9 h-9'>
             <use href='#locIcon'></use>
           </svg>
           Your Location
         </h2>
         <div className="flex justify-between items-center">
+          <form action="" className='w-full'>
+            <div className='flex items-center justify-between gap-4'>
+              <div className='flex flex-col justify-center w-full'>
+                <label htmlFor="">Latitude</label>
+                <input type="text" placeholder='32.6545' className='w-full p-2 rounded border-2 border-gray-200 hover:border-(--primaryColor)'/>
+              </div>
+              <div className='flex flex-col justify-center w-full'>
+                <label htmlFor="">Longitude</label>
+                <input type="text" placeholder='32.6545' className='w-full p-2 rounded border-2 border-gray-200 hover:border-(--primaryColor)'/>
+              </div>
+            </div>
+            <div className='flex-row-center h-[222px] border border-gray-200 my-3'>
+              map
+            </div>
+            <div className='flex items-center justify-center gap-2'>
+              <button className='text-xl w-full cursor-pointer text-red-600 hover:text-white py-2 rounded transition border-2 border-red-600 hover:bg-red-600'>Discard</button>
+              <button className='text-xl w-full cursor-pointer text-white py-2 rounded transition border-2 border-(--primaryColor) bg-(--primaryColor)/90 hover:bg-(--primaryColor)'>Change Location</button>
+            </div>
+          </form>
+
+        </div>
+      </div>
+
+      <div className="bg-white shadow p-3 px-7 mx-3 ml-76 my-4">
+        <h2 className="flex items-center py-3 text-xl text-red-600">
+          <Image src='images/address.svg' height={22} width={22} alt=''></Image>
+          Your Address
+        </h2>
+        <div className="flex justify-between items-center">
+          <form action="" className='w-full'>
+            <div className='flex flex-col items-center justify-between gap-4 mb-3'>
+              <div className='flex items-center justify-between w-full gap-4'>
+                <div className='flex flex-col justify-center w-full'>
+                  <label htmlFor="">German</label>
+                  <input type="text" placeholder='32.6545' className='w-full p-2 rounded border-2 border-gray-200 hover:border-(--primaryColor)'/>
+                </div>
+                <div className='flex flex-col justify-center w-full'>
+                  <label htmlFor="">Berlin</label>
+                  <input type="text" placeholder='32.6545' className='w-full p-2 rounded border-2 border-gray-200 hover:border-(--primaryColor)'/>
+                </div>
+              </div>
+              <div className='flex flex-col justify-center w-full'>
+                <label htmlFor="">Address</label>
+                <input type="text" placeholder='Street name, building, etc.' className='w-full p-2 rounded border-2 border-gray-200 hover:border-(--primaryColor)'/>
+              </div>
+              <div className='flex flex-col justify-center w-full'>
+                <label htmlFor="">Postal Code</label>
+                <input type="text" placeholder='0123456789' className='w-full p-2 rounded border-2 border-gray-200 hover:border-(--primaryColor)'/>
+              </div>
+            </div>
+            <div className='flex items-center justify-center gap-2'>
+              <button className='text-xl w-full cursor-pointer text-red-600 hover:text-white py-2 rounded transition border-2 border-red-600 hover:bg-red-600'>Discard</button>
+              <button className='text-xl w-full cursor-pointer text-white py-2 rounded transition border-2 border-(--primaryColor) bg-(--primaryColor)/90 hover:bg-(--primaryColor)'>Save Address</button>
+            </div>
+          </form>
           
         </div>
       </div>
